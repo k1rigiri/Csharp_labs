5b079866a9113cb92d5851699f029c501ee1d1d6
Viktor <vita-svir1@mail.ru>
2022-03-09 12:58:46 +0300

    init

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -13,7 +13,9 @@ namespace Lab_1
             //действительной и мнимой части
             complex a = new complex() { Real = 3, Imag = 0.5 };
             complex b = new complex() { Real = 2, Imag = 0.8 };
-
+            
+            Student Anton = new Student() { Name = "Anton", Surname = "Ivanov", Sex = "Male", age = 20, mark = 4.6 };
+            Student Dmitry = new Student() { Name = "Dmitry", Surname = "Nagoev", Sex = "Male", age = 22, mark = 4.8 };
             //к числу а прибавим b
             a.Add(b);
 
@@ -35,6 +37,15 @@ namespace Lab_1
 
             Console.WriteLine("a = {0} + {1}i", a.Real, a.Imag);
             Console.WriteLine("b = {0} + {1}i", b.Real, b.Imag);
+
+            Console.WriteLine("\nПервый студент:");
+            Console.WriteLine(" Имя - {0}\n Фамилия - {1}\n Пол - {2}\n Возраст - {3}\n Оценка - {4}", Anton.Name, Anton.Surname, Anton.Sex, Anton.age, Anton.mark);
+            Console.WriteLine("\nВторой студент:");
+            Console.WriteLine(" Имя - {0}\n Фамилия - {1}\n Пол - {2}\n Возраст - {3}\n Оценка - {4}", Dmitry.Name, Dmitry.Surname, Dmitry.Sex, Dmitry.age, Dmitry.mark);
+            Console.WriteLine("\nТеперь сложим студентов:\n");
+
+            Anton.add(Dmitry);
+            Console.WriteLine(" Имена - {0}\n Фамилии - {1}\n Пол - {2}\n Общий возраст - {3}\n Средняя оценка - {4}", Anton.Name, Anton.Surname, Anton.Sex, Anton.age, Anton.mark);
         }
     }
 }
--- Lab_1/Student.cs
+++ Lab_1/Student.cs
@@ -7,8 +7,23 @@ namespace Lab_1
     class Student
     {
         public string Name { get; set; }
+        public string Surname {get; set; }
         public string Sex { get; set; }
         public int age {get; set; }
-        
+        public double mark {get; set; }
+
+        public void add(Student x)
+        {
+            Name = Name + "|" + x.Name;
+            Surname += "|" + x.Surname;
+            if (Sex.Equals(x.Sex))
+                Sex = "Male";
+            else
+            {
+                Sex = "разный";
+            }
+            age += x.age;
+            mark = (mark + x.mark)/2;
+        }
     }
 }
